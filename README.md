2051790bb164ed78457e06ecfe14269adbea8f8c
Martin Montes <martin11lrx@gmail.com>
2025-03-08 20:55:27 +0100

    Added tailscale-routes-fix.sh

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # tailscale-routes-fix
+
 A bash script to override the routing table on Linux once --accept-routes is specified on a network with more than one subnet router (which causes an infinite loop and loss of connectivity on the local IP))
 
-### Inspired by this Ansible role: [ironicbadger/tailscale-routes-fix](https://github.com/ironicbadger/tailscale-routes-fix) 
+__Inspired by this Ansible role: [ironicbadger/tailscale-routes-fix](https://github.com/ironicbadger/tailscale-routes-fix)__
+
+## Usage
+
+```bash
+curl -sfL https://raw.githubusercontent.com/mmontes11/tailscale-routes-fix/main/tailscale-routes-fix.sh | \
+  sudo env LAN_SUBNET="10.0.0.0/24" ROUTES_TABLE="main" bash
+```
+
+This will create a systemd service to setup the IP rule on boot and therefore fixing the routing table for Tailscale.
+
+## Cleanup
+
+In order to remove the IP rule, you can stop the systemd service:
+
+```bash
+sudo systemctl stop tailscale-routes-fix
+```
+
+To fully remove the systemd service:
+
+```bash
+sudo systemctl disable tailscale-routes-fix
+sudo rm /etc/systemd/system/tailscale-routes-fix.service
+```
---
+++ tailscale-routes-fix.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [[ -z "$LAN_SUBNET" ]]; then
+  echo "Error: LAN_SUBNET must be set before running the script."
+  echo "Example:"
+  echo "  export LAN_SUBNET=10.0.0.0/24"
+  exit 1
+fi
+ROUTES_TABLE=${ROUTES_TABLE:-main}
+SERVICE_NAME="tailscale-ip-rule"
+SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+
+echo "Creating systemd service file..."
+sudo tee "$SERVICE_FILE" > /dev/null <<EOF
+[Unit]
+Description=Add custom IP rule at boot for Tailscale
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/sbin/ip rule add to $LAN_SUBNET priority 2500 lookup $ROUTES_TABLE
+ExecStop=/sbin/ip rule del to $LAN_SUBNET priority 2500 lookup $ROUTES_TABLE
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Reloading systemd daemon..."
+systemctl daemon-reload
+
+echo "Enabling systemd service..."
+systemctl enable "$SERVICE_NAME"
+
+echo "Starting systemd service..."
+systemctl start "$SERVICE_NAME"
+
+echo "Verifying IP rule..."
+ip rule show | grep "$LAN_SUBNET" || echo "Rule not found. Please check manually."
+
+echo "Setup complete!"
